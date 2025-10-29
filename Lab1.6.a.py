64ece8a92d41da6035817e8059720909faec7e9c
AndriiBurii <andrburiy@gmail.com>
2025-10-29 15:02:42 +0100

    Lab1.6-Lab1.6.a zmiany

--- Lab1.6.a.py
+++ Lab1.6.a.py
@@ -3,8 +3,8 @@ import random
 drogę = random.randint(10,1000)
 spalanie = float(input("Wpisz średnie spalanie:"))
 cena_palywa =  float(input("Wpisz cena palywa:"))
-zużycie = (spalanie/100)*drogę
+zużycie = float("{:.2f}".format((spalanie/100)*drogę))
 cena_przejazdu = "{:.2f}".format(zużycie*cena_palywa)
 print("Drogę pokonana:", drogę)
 print("Zużycie paliwa:", zużycie, "litrów")
-print("Cena przejazdu:", cena_przejazdu)
+print("Cena przejazdu:", cena_przejazdu, "PLN")
--- Lab1.6.py
+++ Lab1.6.py
@@ -1,7 +1,7 @@
 drogę = int(input("Wpisz drogę pokonaną przez samochód:"))
 spalanie = float(input("Wpisz średnie spalanie:"))
-zużycie = (spalanie/100)*drogę
+zużycie = float("{:.2f}".format((spalanie/100)*drogę))
 cena_palywa =  6.5
-cena_przejazdu = zużycie*cena_palywa
+cena_przejazdu = float("{:.2f}".format(zużycie*cena_palywa))
 print("Zużycie paliwa:", zużycie, "litrów")
-print("Cena przejazdu:", cena_przejazdu)
+print("Cena przejazdu:", cena_przejazdu, "PLN")
